390c33af99990028897d6e903b10bd7809e83c62
Charlotte_gyuan <gy36362@gmail.com>
2016-11-16 16:39:18 +0100

    delete struct

--- trial_division.cpp
+++ trial_division.cpp
@@ -12,16 +12,9 @@ using std::endl;
 #define LINE 100
 
 
-clock_t t;
-
-struct Divisor
-{
-    mpz_class a, b;
-    Divisor(mpz_class a, mpz_class b) : a(a), b(b) {}
-};
 
 
-Divisor trialDivision(mpz_class N,double t0);
+mpz_class trialDivision(mpz_class N,double t0);
 
 int main()
 {
@@ -31,9 +24,8 @@ int main()
 
     int count = 0;
     mpz_class N;
-    struct Divisor ds(0,0);
     while(cin >> N){
-    
+
      int is_prime = mpz_probab_prime_p(N.get_mpz_t(), 10);
      if(is_prime>=1){
            factors[count].push_back(N);
@@ -41,28 +33,30 @@ int main()
            continue;
        }
 
-
+//
 //   for(int jj=0;jj<3;jj++){
 //
 //
 //      cin>>N;
        double t0 = clock();
 
+      mpz_class a,b;
 
       while(true){
+
             int is_prime = mpz_probab_prime_p(N.get_mpz_t(), 10);
             if(is_prime>=1){
                    factors[count].push_back(N);
                    break;
             }
-            ds = trialDivision(N,t0);
-            if(ds.a == -1){
+            a = trialDivision(N,t0);
+            if(a == -1){
                 factors[count].clear();
                 break;
             }else{
-                if(ds.a!=0){
-                    factors[count].push_back(ds.a);
-                    N = ds.b;
+                if(a!=0){
+                    factors[count].push_back(a);
+                    N = N/a;
                 }else{
                     factors[count].push_back(N);
                     break;
@@ -100,32 +94,32 @@ int main()
 }
 
 
-Divisor trialDivision(mpz_class N, double t0){
+mpz_class trialDivision(mpz_class N, double t0){
 
-    Divisor divisor(0,0);
+    mpz_class a=0;
+    if (N % 2 == 0) {
+            return 2;
+    }
 
     mpz_class sqr = sqrt(N);
     mpz_class rop = sqr - (sqr % 1);
 
 
     for(mpz_class s = 2; s<= rop;s++){
-        
-        
+
+
 
         if(((clock()-(float)t0)/CLOCKS_PER_SEC > 0.18) && (N> 10000000000000000)){
 
 
-            return Divisor(-1,0);
+            return -1;
         }
         if(mpz_divisible_p(N.get_mpz_t(), s.get_mpz_t()) != 0){
 
-            divisor.a = s;
-            divisor.b = N/s;
-            break;
+            return s;
         }
 
 
     }
-    return divisor;
 
 }
